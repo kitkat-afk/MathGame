7a1c243605ff1796efab60884a692da968b8ee33
Ben Williams <56005123+kitkat-afk@users.noreply.github.com>
2019-10-04 15:38:05 -0700

    Created the MathObject barebones class

---
+++ MathObject.java
@@ -0,0 +1,12 @@
+public class MathObject {
+    private int one, two, sum;
+    private String op;
+
+    public MathObject(digitOne, operand, digitTwo, result)
+    {
+        one = digitOne;
+        op = operand;
+        two = digitTwo;
+        sum = result;
+    }
+}
