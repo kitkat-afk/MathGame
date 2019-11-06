2317d1f1e3109165cc69a5a664269ea57698fdb3
Ben Williams <56005123+kitkat-afk@users.noreply.github.com>
2019-11-06 15:11:30 -0800

    11/6 Update

--- src/Database.java
+++ src/Database.java
@@ -1,81 +1,42 @@
-
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
- * This class creates the database of the whole game, storing all the characters
- * into a file. It will search the file if the username exists. If it does, it
- * will load that character. If it does not, it will write a new file with the
- * previous characters.
+ * Database class for interfacing with an SQLite database file.
+ *
+ * @author Benjamin Williams
+ * @version 11.02.19
  */
 
-@SuppressWarnings("unchecked")
 public class Database {
-    public static ArrayList<Account> files = new ArrayList<Account>();
-    public static final String FILE_NAME = "StudentList";
+        /**
+         * Connect to a sample database
+         */
+        public static void connect() {
+            Connection conn = null;
+            try {
+                // db parameters
+                String url = "jdbc:sqlite:db.sqlite";
+                // create a connection to the database
+                conn = DriverManager.getConnection(url);
 
-    /**
-     * This method will return either the index in the ArrayList if the character is
-     * in the file or it will return -1 if the character is not found.
-     *
-     * @return Index in the ArrayList or -1 if not found
-     */
-    public static int inFile() {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("What is your username?");
-        String input = keyboard.nextLine();
+                System.out.println("Connection to SQLite has been established.");
 
-        for (int i = 0; i < files.size(); i++) {
-            // Checking if the username is in the system
-            if (files.get(i).username.equals(input) == true) {
-                return i;
-            } else {
-                System.out.println("Oops, your username is not in the system");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
         }
-        return -1;
+
+    public static void main(String[] args) {
+        connect();
     }
-
-    /**
-     * This method creates a new file, overwriting the old one with the past and
-     * previous information if the character is not found in the ArrayList. If the
-     * character found, it will return the character.
-     *
-     * @return The character the user creates or character in the file
-     */
-    public static Account createFile() {
-        Account student = null;
-        try {
-            File newFile = new File(FILE_NAME);
-            Scanner keyboard = new Scanner(System.in);
-            Scanner scan = new Scanner(newFile);
-            String input = " ";
-
-            // scanning the file and adding the names in
-            while (scan.hasNext() == true) {
-                files.add(new Account(scan.nextLine()));
-            }
-
-
-            if (!(inFile() == (-1))) {
-                int i = inFile();
-                student = files.get(i);
-            } else {
-                // adding the character to the arraylist
-                student = new Account(input);
-                files.add(student);
-                ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(newFile));
-
-                // writing the arraylist to the file
-                outputStream.writeObject(files.toString());
-                outputStream.close();
-            }
-
-        } catch (Exception IOException) {
-            System.exit(1);
-        }
-        return student;
-    }
-
 }
