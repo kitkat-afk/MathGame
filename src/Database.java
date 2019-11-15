aaec97e8cea0f0f7283955bbbdffdef3dac48ec4
Ben Williams <56005123+kitkat-afk@users.noreply.github.com>
2019-11-15 15:31:55 -0800

    11/15 Database Incrimental Update

--- src/Database.java
+++ src/Database.java
@@ -21,7 +21,7 @@ public class Database {
         try {
             con = DriverManager.getConnection(url);
             System.out.println("Database connected.");
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -30,23 +30,19 @@ public class Database {
     }
 
     public void login(String username, String password) {
-        String sql = ("SELECT * FROM Students WHERE username = " + username + " AND password = " + password + ";");
+        String sql = ("SELECT * FROM Students WHERE username = ? AND password = ?;");
 
-        try (Connection con = this.connect(); Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+        try (Connection con = this.connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1,username);
+            pstmt.setString(2,password);
+
+            ResultSet rs = pstmt.executeQuery();
+
             while (rs.next()) {
-                //user found in database
-                if (!rs.wasNull()) {
-                    Student result = new Student(rs.getString("name"), rs.getString("username"), rs.getString("password"));
-                    result.setAnsAttempt(rs.getInt("attempt"));
-                    result.setAnsCorrect(rs.getInt("correct"));
-
-                    System.out.println(result);
-                }
-
-                //user not found in database
-                else {
-                    System.out.println("User not found.");
-                }
+                Student result = new Student(rs.getString("name"), rs.getString("username"), rs.getString("password"));
+                result.setAnsAttempt(rs.getInt("attempt"));
+                result.setAnsCorrect(rs.getInt("correct"));
+                System.out.println(result);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -55,7 +51,9 @@ public class Database {
 
     public void newUser(String name, String user, String pass) {
         String sql = ("INSERT INTO Students(name, username, password, correct, attempt) VALUES("
-                + name + "," + user + "," + pass + ", 0, 0);");
+                + name + ", " + user + ", " + pass + ", 0, 0);");
+
+        System.out.println(sql);
 
         try (Connection con = this.connect(); Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
         } catch (SQLException e) {
@@ -64,11 +62,34 @@ public class Database {
     }
 
 
+    public void printAllStudents() {
+        String sql = "SELECT * FROM Students";
+
+        try(Connection con = this.connect(); PreparedStatement ptsmt = con.prepareStatement(sql)) {
+            ResultSet rs = ptsmt.executeQuery();
+
+            while(rs.next()) {
+                System.out.println(rs.getInt("ID") + "\t" +
+                        rs.getString("name")  + "\t" +
+                        rs.getString("username") + "\t" +
+                        rs.getString("password") + "\t" +
+                        rs.getInt("correct") + "\t" +
+                        rs.getInt("attempt"));
+            }
+
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args) {
         Database db = new Database();
-
-        db.login("tester","pass");
-
+        db.printAllStudents();
+        db.login("jdoe","deer");
+        db.newUser("Jane Doe","jdoe","deer");
+        db.printAllStudents();
     }
 
 }
