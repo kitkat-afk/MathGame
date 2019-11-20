aad84b37acef57069299d1cc030246601178bd9a
Ben Williams <56005123+kitkat-afk@users.noreply.github.com>
2019-11-20 15:34:23 -0800

    THE GREAT DATABASE REVELATION

--- src/Database.java
+++ src/Database.java
@@ -29,7 +29,7 @@ public class Database {
 
     }
 
-    public void login(String username, String password) {
+    public Student login(String username, String password) {
         String sql = ("SELECT * FROM Students WHERE username = ? AND password = ?;");
 
         try (Connection con = this.connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
@@ -42,21 +42,32 @@ public class Database {
                 Student result = new Student(rs.getString("name"), rs.getString("username"), rs.getString("password"));
                 result.setAnsAttempt(rs.getInt("attempt"));
                 result.setAnsCorrect(rs.getInt("correct"));
-                System.out.println(result);
+                return result;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        //if nothing found
+        return null;
     }
 
     public void newUser(String name, String user, String pass) {
-        String sql = ("INSERT INTO Students(name, username, password, correct, attempt) VALUES("
-                + name + ", " + user + ", " + pass + ", 0, 0);");
+        String sql = ("INSERT INTO Students(name, username, password, correct, attempt) VALUES(\'"
+                + name + "\', \'" + user + "\', \'" + pass + "\', 0, 0);");
 
         System.out.println(sql);
 
-        try (Connection con = this.connect(); Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
-        } catch (SQLException e) {
+        Connection c = this.connect();
+        Statement s = null;
+
+        try {
+            s = c.createStatement();
+            s.executeUpdate(sql);
+            s.close();
+            c.close();
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }
@@ -83,12 +94,31 @@ public class Database {
         }
     }
 
+    public void deleteStudent(String user)
+    {
+        String sql = ("DELETE FROM Students WHERE username = \'" +  user + "\';");
+        System.out.println(sql);
+
+        Connection c = this.connect();
+        Statement s = null;
+
+        try {
+            s = c.createStatement();
+            s.executeUpdate(sql);
+            s.close();
+            c.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     public static void main(String[] args) {
         Database db = new Database();
         db.printAllStudents();
-        db.login("jdoe","deer");
-        db.newUser("Jane Doe","jdoe","deer");
+        db.login("jdoe","me");
         db.printAllStudents();
     }
 
